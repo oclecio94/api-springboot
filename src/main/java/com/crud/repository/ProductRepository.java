a35a780211271c41b779338a2255fb80eab05918
Oclecio Alves <oclecioalvessousa@gmail.com>
2023-08-30 16:58:10 -0300

    first commit

---
+++ src/main/java/com/crud/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.crud.repository;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+
+import com.crud.models.ProductModel;
+
+//@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+    
+}
